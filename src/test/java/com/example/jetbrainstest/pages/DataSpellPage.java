65a1e06f317a0c3010598171733ca349b554f1c4
Rodionchik73 <olelik260591@gmail.com>
2025-02-08 23:30:53 +0300

    added tests for DataSpellPage

--- src/test/java/com/example/jetbrainstest/pages/DataSpellPage.java
+++ src/test/java/com/example/jetbrainstest/pages/DataSpellPage.java
@@ -9,12 +9,59 @@ import org.openqa.selenium.support.PageFactory;
 public class DataSpellPage {
     WebDriver driver;
 
-    @FindBy(xpath = "//span/text()[. =\'Pricing\']")
+    @FindBy(css = "a[href='/dataspell/buy/']")
     private WebElement pricingButton;
+
+    @FindBy(css = "button[aria-label=\'Open search\']")
+    private WebElement searchButton;
+
+    @FindBy(css = "button[data-test='full-search-button']")
+    private WebElement fullSearchButton;
+
+    @FindBy(css = "[data-test-marker='Developer Tools']")
+    private WebElement developerTools;
+
+    @FindBy(xpath = "//*[contains(@data-test, 'main-submenu-column-title')] [text() = 'JETBRAINS IDEs']")
+    private WebElement jetbrainsIdesMenu;
+
+    @FindBy(css = "[href='/dataspell/whatsnew/']")
+    private WebElement whatsNewButton;
 
     public Boolean checkPricingButtonClickable() {
         System.out.println("Проверка активности кнопки Pricing");
         return pricingButton.isEnabled();
+    }
+
+    public boolean clickSearchButton() {
+        System.out.println("Клик на Developer Tools");
+        return searchButton.isEnabled();
+    }
+
+    public void sendWordInSearchButton(String text) {
+        searchButton.click();
+        searchButton.sendKeys(text);
+        System.out.println("Ввод слова в строке поиска");
+    }
+
+    public Boolean clickFullSearchButton() {
+        System.out.println("Клик на кнопку Advanced search Ctrl+K");
+        return fullSearchButton.isDisplayed();
+    }
+
+    public boolean clickDeveloperTools() {
+        System.out.println("Клик на Developer Tools");
+        return developerTools.isEnabled();
+    }
+
+    public boolean jetbrainsIdesMenuIsDisplayed() {
+        System.out.println("JETBRAINS IDEs отображается");
+        return jetbrainsIdesMenu.isEnabled();
+    }
+
+    public String whatsNewUrl() {
+        System.out.println("Проверка URL страницы после клика на WhatsNew");
+        whatsNewButton.click();
+        return driver.getCurrentUrl();
     }
 
     public DataSpellPage(WebDriver driver) {
--- src/test/java/com/example/jetbrainstest/tests/DataSpellTest.java
+++ src/test/java/com/example/jetbrainstest/tests/DataSpellTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class DataSpellTest extends BaseTest{
+public class DataSpellTest extends BaseTest {
     private DataSpellPage dataSpellPage;
 
     @BeforeEach
@@ -24,4 +25,43 @@ public class DataSpellTest extends BaseTest{
     public void buttonPricing() {
         assertTrue(dataSpellPage.checkPricingButtonClickable(), "Кнопка Pricing не активна");
     }
+
+    @Test
+    @DisplayName("Проверка, что значок лупы активен")
+    public void searchButton() {
+        assertTrue(dataSpellPage.clickSearchButton(), "Кнопка Search не активна");
+    }
+
+    /*в этом тексте почему-то текст не вводится, и показывается на странице аксепт куки, подскажи пожалуйста что не так?*/
+    @Test
+    @DisplayName("Проверка что отображается кнопка 'Advanced search Ctrl+K' после ввода значения в строку поиска")
+    public void advancedSearch() {
+        String input = "ai";
+        dataSpellPage.sendWordInSearchButton(input);
+        assertTrue(dataSpellPage.clickFullSearchButton(), "Кнопка 'Advanced search Ctrl+K' не отображается");
+    }
+
+    @Test
+    @DisplayName("Проверка, что кнопка Developer Tools активна")
+    public void developerToolsButton() {
+        assertTrue(dataSpellPage.clickDeveloperTools(), "Кнопка Developer Tools не активна");
+    }
+
+    /*как правильнее сделать если я хочу сразу проверить что 4 блока отображается? нужно 4 теста создать? или в одном как-то можно сделать?*/
+    @Test
+    @DisplayName("После клика на кнопку Developer Tool отображается четыре блока: JETBRAINS IDEs, PLUGINS & SERVICES, .NET & VISUAL STUDIO, LANGUAGES & FRAMEWORKS ")
+    public void menuOfDeveloperTools() {
+        dataSpellPage.clickDeveloperTools();
+        assertTrue(dataSpellPage.jetbrainsIdesMenuIsDisplayed(), "JETBRAINS IDEs не отображается");
+
+    }
+
+    @Test
+    @DisplayName("После клика на кнопку Whatsnew переходит на страницу https://www.jetbrains.com/dataspell/whatsnew/")
+    public void whatsNewUrl() {
+        assertEquals("https://www.jetbrains.com/dataspell/whatsnew/", dataSpellPage.whatsNewUrl(), "Не верная url после клика на Whats new");
+    }
+
+
 }
+
